07cf240b7dba6be3b022b6ec842e816e761b8955
Gabriel Delattre <gabriel@soixantecircuits.fr>
2012-11-27 20:27:27 +0100

    improving template mail

--- admin/view/ws_config.php
+++ admin/view/ws_config.php
@@ -1,11 +1,11 @@
 <div class="ws_warp">
-	<?php $generalConfig = $this->get_Manager()->getGeneralConfig(); ?>
-	<div id="icon-options-general" class="icon32"><br></div><div class="page_title"><h1><?php _e("General Configurations","ws"); ?></h1></div>
-	<div class="cb"></div>
-	<form method="POST" action="?page=<?php echo $this->configPageName; ?>&WS_action=update" >
-		<?php require_once("ws_config_inputs.php"); ?>
-		<div class="submit">
-			<input class="btn btn-success" type="submit" value="<?php _e('Save','ws'); ?>" /><br/>
-		</div>
-	</form>
+  <?php $generalConfig = $this->get_Manager()->getGeneralConfig(); ?>
+  <div id="icon-options-general" class="icon32"><br></div><div class="page_title"><h1><?php _e("General Configurations","ws"); ?></h1></div>
+  <div class="cb"></div>
+  <form method="POST" class="form-inline " action="?page=<?php echo $this->_configPageName; ?>&WS_action=update" >
+    <?php require_once("ws_config_inputs.php"); ?>
+    
+      <input class="btn btn-success" id='submit' type="submit" value="<?php _e('Save', 'ws'); ?>" /><br/>
+    
+  </form>
 </div>
